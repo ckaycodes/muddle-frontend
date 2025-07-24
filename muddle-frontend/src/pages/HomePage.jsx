34ff4da6f5c4bde6cc568f7d1d786375c642876a
ckay <chandlercook101@gmail.com>
2025-07-23 20:07:31 -0700

    Logo changes

--- muddle-frontend/src/pages/HomePage.jsx
+++ muddle-frontend/src/pages/HomePage.jsx
@@ -9,12 +9,12 @@ function HomePage() {
   return (
     <>
       <div className="min-h-screen bg-emerald-100 flex items-center justify-center px-4 pt-24">
-        <div className="max-w-3xl w-full bg-white rounded-xl shadow-md p-8 text-center">
+        <div className="max-w-xl w-full bg-white rounded-md shadow-xl p-2 text-center">
           {/* Logo & Title */}
-          <div className="mb-6">
-            <img src="/M.png" alt="Muddle Logo" className="mx-auto w-28 sm:w-36 md:w-44 lg:w-52 h-auto mb-2 max-w-full" />
+          <div className="mb-2">
+            <img src="/Muddle.png" alt="Muddle Logo" className="mx-auto w-50 sm:w-40 md:w-44 lg:w-52 h-auto mb-2 max-w-full" />
             <h1 className="text-4xl font-extrabold text-emerald-700">
-              Welcome to Muddle!
+        
             </h1>
             {isLoggedIn && user && (
               <p className="mt-2 text-gray-600 text-lg">
